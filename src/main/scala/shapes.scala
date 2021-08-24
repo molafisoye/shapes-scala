cab80d76d064572135351aca9a11ed63cd09f832
Konstantin Läufer <klaeufer@gmail.com>
2021-08-24 13:56:10 -0500

    removed unnecessary comment
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -10,4 +10,3 @@ case class Rectangle(width: Int, height: Int) extends Shape
 case class Location(x: Int, y: Int, shape: Shape) extends Shape
 
 // TODO add missing case classes (see test fixtures)
-// TODO must include validity checking for constructor arguments
