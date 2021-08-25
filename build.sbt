bb5398eaa5d2b4eea4fc0d99805dcfbc37ad6c3a
Konstantin Läufer <klaeufer@gmail.com>
2021-08-25 17:35:04 -0500

    enabled typesafe multiversal equality
    Signed-off-by: Konstantin Läufer <klaeufer@gmail.com>

--- build.sbt
+++ build.sbt
@@ -4,7 +4,7 @@ version := "0.4"
 
 scalaVersion := "3.0.1"
 
-scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Yexplicit-nulls")
+scalacOptions ++= Seq("-deprecation", "-feature", "-unchecked", "-Yexplicit-nulls", "-language:strictEquality")
 
 libraryDependencies ++= Seq(
   "org.scalatest"  %% "scalatest"  % "3.2.9"  % Test,
--- src/test/scala/TestBoundingBox.scala
+++ src/test/scala/TestBoundingBox.scala
@@ -10,10 +10,10 @@ class TestBoundingBox extends AnyFunSuite {
   def testBoundingBox(description: String, s: Shape, x: Int, y: Int, width: Int, height: Int) = {
     test(description) {
       val Location(u, v, Rectangle(w, h)) = boundingBox(s)
-      assert(x === u)
-      assert(y === v)
-      assert(width === w)
-      assert(height === h)
+      assert(x == u)
+      assert(y == v)
+      assert(width == w)
+      assert(height == h)
     }
   }
 
