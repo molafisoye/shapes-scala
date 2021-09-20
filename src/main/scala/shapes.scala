7435da6209e234b350ddffc3195b53baa8669b88
mckenziet088 <91046902+mckenziet088@users.noreply.github.com>
2021-09-20 02:57:00 -0500

    Update shapes.scala

--- src/main/scala/shapes.scala
+++ src/main/scala/shapes.scala
@@ -3,6 +3,8 @@ package edu.luc.cs.laufer.cs371.shapes
 /** data Shape = Rectangle(w, h) | Location(x, y, Shape) */
 enum Shape {
   case Rectangle(width: Int, height: Int)
+  case Ellipse(halfWidth: Int, halfHeight: Int)
   case Location(x: Int, y: Int, shape: Shape)
-  // TODO add missing cases (see test fixtures)
+  case Group(shapes: Shape*)
+  // DONE add missing cases (see test fixtures)
 }
