2578846d3dcca655a144d11e139304f6dfcc092f
mckenziet088 <91046902+mckenziet088@users.noreply.github.com>
2021-09-20 02:58:08 -0500

    Update TestBoundingBox.scala

--- src/test/scala/TestBoundingBox.scala
+++ src/test/scala/TestBoundingBox.scala
@@ -1,26 +1,36 @@
 package edu.luc.cs.laufer.cs371.shapes
 
-import org.scalatest.funsuite.AnyFunSuite
-
-import TestFixtures.*
 import Shape.*
 
-class TestBoundingBox extends AnyFunSuite {
+object TestFixtures {
 
-  def testBoundingBox(description: String, s: Shape, x: Int, y: Int, width: Int, height: Int) =
-    test(description) {
-      val Location(u, v, Rectangle(w, h)) = boundingBox(s)
-      assert(x == u)
-      assert(y == v)
-      assert(width == w)
-      assert(height == h)
-    }
+  // DONE comment these fixtures back in after adding the required case classes
 
-  // DONE comment these tests back in
-  testBoundingBox("simple ellipse", simpleEllipse, -50, -30, 100, 60)
-  testBoundingBox("simple rectangle", simpleRectangle, 0, 0, 80, 120)
-  testBoundingBox("simple location", simpleLocation, 70, 30, 80, 120)
-  testBoundingBox("basic group", basicGroup, -50, -30, 100, 70)
-  testBoundingBox("simple group", simpleGroup, 150, 70, 350, 280)
-  testBoundingBox("complex group", complexGroup, 30, 60, 470, 320)
+  val simpleEllipse = Ellipse(50, 30)
+
+  val simpleRectangle = Rectangle(80, 120)
+
+  val simpleLocation = Location(70, 30, Rectangle(80, 120))
+
+  val basicGroup = Group(Ellipse(50, 30), Rectangle(20, 40))
+
+  val simpleGroup = Group(
+    Location(200, 100, Ellipse(50, 30)),
+    Location(400, 300, Rectangle(100, 50))
+  )
+
+  val complexGroup =
+    Location(50, 100,
+      Group(
+        Ellipse(20, 40),
+        Location(150, 50,
+          Group(
+            Rectangle(50, 30),
+            Rectangle(300, 60),
+            Location(100, 200,
+              Ellipse(50, 30)
+            )
+          )),
+        Rectangle(100, 200)
+      ))
 }
