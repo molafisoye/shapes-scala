279c29ceede01eb160955d4a12aea42920b30292
mckenziet088 <91046902+mckenziet088@users.noreply.github.com>
2021-09-20 17:39:59 -0500

    Update boundingBox.scala

--- src/main/scala/boundingBox.scala
+++ src/main/scala/boundingBox.scala
@@ -9,3 +9,4 @@ object boundingBox {
     case Rectangle(w,h) => Location(0,0, Rectangle(w,h))
     case _ => Location(0, 0, Rectangle(0, 0)) // implemented
 }
+// Group is the hardest (try not to reinvent the algorith for group use existing algorithm)
